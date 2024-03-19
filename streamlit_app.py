28caae53f124e305ca61ed1696946b647832ed9e
skishore2708 <163954572+skishore2708@users.noreply.github.com>
2024-03-18 23:45:59 -0400

    Update streamlit_app.py
    Adding the NF info into the block of fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -11,8 +13,6 @@ st.write(
 
 name_on_Order = st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be ', name_on_Order)
-
-from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -31,9 +31,8 @@ if ingredients_list:
 
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
-
-    #st.write(ingredients_string)
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_dv=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order,Order_Filled)
             values ('""" + ingredients_string + """','""" + name_on_Order+ """',0)""" 
@@ -45,7 +44,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_Order +"!" , icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_dv=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
